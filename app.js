f77b20560a44c816a31a58e951188ac910809368
Nathan Goodman <nathan.something@gamil.com>
2016-01-23 16:10:18 -0500

    added form functionality

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 
 var express = require('express');
 var path = require('path');
+var Api = require("rosette-api").Api;
+var ArgumentParser = require("argparse").ArgumentParser;
+var DocumentParameters = require("rosette-api").DocumentParameters;
 var app = express();
+
+var parser = new ArgumentParser({
+	  addHelp: true,
+	  description: "Get the words in a piece of text"
+	});
+	parser.addArgument(["--key"], {help: "Rosette API key", required: true});
+
+var basis_key = parser.parseArgs().key;
+var basis_api = new Api(basis_key);
 
 app.set('port', process.env.PORT || 3000);
 app.engine('jshtml', require('jshtml-express'));
@@ -29,14 +41,43 @@ app.get('/register', function (request, result) {
 });
 
 app.post('/', function (request, result) {
-	console.log(request.body.user.firstname);
-	console.log(request.body.user.lastname);
-	console.log(request.body.user.email);
-	console.log(request.body.user.phonenumber);
-	result.locals({
-		title : 'Hack Connections'
+	console.log('first name: ' + request.body.user.firstname);
+	console.log('last name: ' + request.body.user.lastname);
+	console.log('email: ' + request.body.user.email);
+	console.log('phone number: ' + request.body.user.phonenumber);
+	"use strict";
+
+	basis_api.tokens(request.body.user.bio, function(error, result) {
+	  if (error) {
+	    throw error;
+	  }
+	  else {
+	     console.log(result);
+	  }
 	});
-	result.render('index');
+
+	basis_api.tokens(request.body.user.tech_background, function(error, result) {
+	  if (error) {
+	    throw error;
+	  }
+	  else {
+	     console.log(result);
+	  }
+	});
+
+	basis_api.tokens(request.body.user.project, function(error, result) {
+	  if (error) {
+	    throw error;
+	  }
+	  else {
+	     console.log(result);
+	  }
+	});
+
+	result.locals({
+		title : 'Browsing'
+	});
+	result.render('browsing');
 });
 
 // app.get('/done', function(request, result) {
