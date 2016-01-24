36065bed8b6a01ea1d5040cce0e50a8db0c9e48f
Logan Moore <lmooore@gmail.com>
2016-01-23 23:46:30 -0500

    Database functionality nearly done

--- db/dbInterface.js
+++ db/dbInterface.js
@@ -2,49 +2,45 @@ var StringBuilder = require('stringbuilder');
 var fs = require('fs');
 var mysql = require('mysql');
 
-exports.getPersonAttribute = function(connection, fullname, attribute) {
+exports.getPersonAttribute = function(dbConnection, value, attribute, callback) {
   var sb = new StringBuilder();
 
-  sb.append('SELECT ');
+  sb.append('SELECT * FROM `People` WHERE `');
   sb.append(attribute);
-  sb.append(' FROM people WHERE fullname = \'');
-  sb.append(fullname);
-  sb.append('\';');
+  sb.append('` = "');
+  sb.append(value);
+  sb.append('";');
 
   var toReturn = undefined;
 
-  sb.build(function(err, result) {
-    console.log("Query: " + result);
-    toReturn = result;
+  return sb.build(function(err, result) {
+    // console.log("Query: " + result);
+    return dbConnection.query(result, callback);
   });
-
-  return toReturn;
 }
 
-exports.getAttributeValues = function(dbConnection, attribute) {
+exports.getAttributeValues = function(dbConnection, attribute, callback) {
   var sb = new StringBuilder();
 
   sb.append('SELECT ');
   sb.append(attribute);
-  sb.append(' FROM people;');
+  sb.append(' FROM People;');
 
   var toReturn = undefined;
 
-  sb.build(function(err, result) {
-    console.log("Query: " + result);
-    toReturn = dbConnection.query(result);
+  return sb.build(function(err, result) {
+    // console.log("Query: " + result);
+    return dbConnection.query(result, callback);
   });
-
-  return toReturn;
 }
 
 // Adds a new person with the given attributes to the database
-exports.addPerson = function(dbConnection, fullname, email, phone, idea, interests, languages) {
+exports.addPerson = function(dbConnection, fullname, email, phone, idea, interests, languages, callback) {
   var insert = new StringBuilder();
   var values = new StringBuilder();
 
-  insert.append('INSERT INTO people \( \`fullname\`, \`email\`, \`phone\`, \`idea\`');
-      values.append('VALUES (');
+  insert.append('INSERT INTO People ( `fullname`, `email`, `phone`, `idea`');
+      values.append(' VALUES (');
       values.append(fullname);
       values.append(', ');
       values.append(email);
@@ -54,55 +50,112 @@ exports.addPerson = function(dbConnection, fullname, email, phone, idea, interes
       values.append(idea);
 
       for (var i in interests) {
-        console.log("i: " + i);
-        if (i != undefined) {
-          appendTo(insert, values, i);
-        }
+        // console.log(i + ": " + interests[i]);
+        appendTo(insert, values, interests[i]);
       }
 
-      for (var i in languages) {
-        console.log("i: " + i);
-        if (i != undefined) {
-          appendTo(insert, values, i);
-        }
+      for (var l in languages) {
+        // console.log(i + ": " + languages[l]);
+        appendTo(insert, values, languages[l]);
       }
 
       insert.append(')');
       values.append(');');
 
-      values.build(function(err, result) {
-        console.log("Values: ", result);
-        insert.append(result);
-      });
-
-      var toReturn = undefined;
+      var insert_s = "";
+      var values_s = "";
 
       insert.build(function(err, result) {
-        console.log("Query: " + result);
-        toReturn = dbConnection.query(result);
-      });
+        // console.log("Insert: ", result);
+          insert_s = result;
+        });
 
-return toReturn;
+      values.build(function(err, result) {
+        // console.log("Values: ", result);
+          values_s = result;
+        });
+
+      var result = insert_s + values_s;
+
+      return dbConnection.query(result, callback);
 }
 
-function appendTo(insert, values, i) {
+function appendTo(insert, values, item) {
   insert.append(', `');
-  insert.append(i);
+  insert.append(item);
   insert.append('`');
   values.append(', ');
-  values.append(i);
+  values.append(item);
+}
+
+exports.getPerson = function(dbPool, email, callback) {
+  return exports.getPersonAttribute(dbPool, email, 'email', function(err, rows) {
+    return {
+    'email': email,
+      'fullname': rows.fullname,
+      'phone': rows.phone
+    };
+  });
+}
+
+exports.getAllPeople = function (dbPool, callback) {
+return exports.getAttributeValues(dbPool, 'email', function(err, rows) {
+    console.log("\nEMAILS");
+    console.error(err);
+    console.log(rows);
+    
+    return rows.map(function(currentValue, index, array) {
+      console.log(currentValue.email);
+      var result = exports.getPerson(dbPool, currentValue.email, function(err, result) {
+        console.log(result);
+        return result;
+      });
+    });
+  });
 }
 
 // Adds a match between the 2 given people
-exports.addMatch = function(name, matchName, matchRank) {
+exports.addMatch = function(dbPool, name, matchName, matchRank, callback) {
   var sb = new StringBuilder();
+
+  sb.append('INSERT INTO Matches VALUES (');
+      sb.append(name);
+      sb.append(', ');
+      sb.append(matchName);
+      sb.append(', ');
+      sb.append(matchRank);
+      sb.append(');');
+
+  var toReturn = undefined;
+
+  return insert.build(function(err, result) {
+    if (err) throw err;
+    // console.log("Query: " + result);
+    console.log(dbConnection.threadId);
+    dbPool.query(result, callback);
+  });
 }
 
-exports.defaultConnection = function() {
-  return mysql.createConnection({
+exports.getMatches = function(dbPool, name, callback) {
+  var sb = new StringBuilder();
+
+  sb.append('SELECT * FROM Matches WHERE name = ');
+  sb.append(pname);
+  sb.append(';');
+
+  return insert.build(function(err, result) {
+    if (err) throw err;
+    // console.log("Query: " + result);
+    console.log(dbConnection.threadId);
+    return dbConnection.query(result, callback);
+  });
+}
+
+exports.defaultPool = function() {
+  return mysql.createPool({
+    connectionLimit: 10,
     host: 'localhost',
          user: 'hack',
-         protocol: 'TCP',
          password: fs.readFileSync('./db/pw.txt').toString().trim(),
          database: 'HackATeam'
   });
