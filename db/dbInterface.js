d8afa1a572b3df165a05e2618266165e0d75648e
Logan Moore <lmooore@gmail.com>
2016-01-24 01:12:35 -0500

    A bit further on database stuff
    Still dealing with errors

--- db/dbInterface.js
+++ db/dbInterface.js
@@ -2,20 +2,23 @@ var StringBuilder = require('stringbuilder');
 var fs = require('fs');
 var mysql = require('mysql');
 
-exports.getPersonAttribute = function(connection, fullname, attribute) {
+exports.getPersonAttribute = function(dbConnection, email, attribute) {
   var sb = new StringBuilder();
 
   sb.append('SELECT ');
   sb.append(attribute);
-  sb.append(' FROM people WHERE fullname = \'');
-  sb.append(fullname);
+  sb.append(' FROM people WHERE email = \'');
+  sb.append(email);
   sb.append('\';');
 
   var toReturn = undefined;
 
   sb.build(function(err, result) {
     console.log("Query: " + result);
-    toReturn = result;
+    dbConnection.query(result, function(err, rows) {
+      toReturn = rows[0];
+      console.log(rows.sql);
+    });
   });
 
   return toReturn;
@@ -32,7 +35,10 @@ exports.getAttributeValues = function(dbConnection, attribute) {
 
   sb.build(function(err, result) {
     console.log("Query: " + result);
-    toReturn = dbConnection.query(result);
+    dbConnection.query(result, function(err, rows) {
+      toReturn = rows;
+      console.log(rows.sql);
+    });
   });
 
   return toReturn;
@@ -43,8 +49,8 @@ exports.addPerson = function(dbConnection, fullname, email, phone, idea, interes
   var insert = new StringBuilder();
   var values = new StringBuilder();
 
-  insert.append('INSERT INTO people \( \`fullname\`, \`email\`, \`phone\`, \`idea\`');
-      values.append('VALUES (');
+  insert.append('INSERT INTO People ( `fullname`, `email`, `phone`, `idea`');
+      values.append(' VALUES (');
       values.append(fullname);
       values.append(', ');
       values.append(email);
@@ -54,35 +60,36 @@ exports.addPerson = function(dbConnection, fullname, email, phone, idea, interes
       values.append(idea);
 
       for (var i in interests) {
-        console.log("i: " + i);
-        if (i != undefined) {
-          appendTo(insert, values, i);
-        }
+        console.log(i + ": " + interests[i]);
+        appendTo(insert, values, interests[i]);
       }
 
       for (var i in languages) {
-        console.log("i: " + i);
-        if (i != undefined) {
-          appendTo(insert, values, i);
-        }
+        console.log(i + ": " + languages[i]);
+        appendTo(insert, values, interests[i]);
       }
 
       insert.append(')');
       values.append(');');
 
-      values.build(function(err, result) {
-        console.log("Values: ", result);
-        insert.append(result);
-      });
-
-      var toReturn = undefined;
+      var insert_s = undefined;
+      var values_s = undefined;
 
       insert.build(function(err, result) {
-        console.log("Query: " + result);
-        toReturn = dbConnection.query(result);
-      });
+        console.log("Insert: ", result);
+          insert_s = result;
+        });
 
-return toReturn;
+      values.build(function(err, result) {
+        console.log("Values: ", result);
+          values_s = result;
+        });
+
+      var result = insert_s + values_s;
+
+      dbConnection.query(result, function(err, rows) {
+        return rows.sql;
+      });
 }
 
 function appendTo(insert, values, i) {
@@ -93,11 +100,35 @@ function appendTo(insert, values, i) {
   values.append(i);
 }
 
+exports.getPerson = function(dbConnection, email) {
+  return {
+    'fullname': getPersonAttribute(dbConnection, email, 'fullname'),
+    'email': email,
+    'phone': getPersonAttribute(dbConnection, email, 'phone')
+  };
+}
+
 // Adds a match between the 2 given people
 exports.addMatch = function(name, matchName, matchRank) {
   var sb = new StringBuilder();
 
-  sb.append('INSERT INTO Matches');
+  sb.append('INSERT INTO Matches VALUES (');
+      sb.append(name);
+      sb.append(', ');
+      sb.append(matchName);
+      sb.append(', ');
+      sb.append(matchRank);
+      sb.append(');');
+
+  var toReturn = undefined;
+
+  insert.build(function(err, result) {
+    if (err) throw err;
+    console.log("Query: " + result);
+    dbConnection.query(result, function (err, out) {
+      return out;
+    });
+  });
 }
 
 exports.defaultConnection = function() {
--- db/dbTest.js
+++ db/dbTest.js
@@ -5,8 +5,7 @@ var dbConnection = dbInterface.defaultConnection();
 
 console.log(dbInterface.getAttributeValues(dbConnection, 'email'));
 console.log(dbInterface.getPersonAttribute(dbConnection, 'Bruce Wayne', 'email'));
-console.log(dbInterface.addPerson(dbConnection, 'John Deere', 'weed@wha.ck', 7605555555, 'grass',
-      {'frontend': true, 'backend': true, 'machineLearn': true}, {'java': true}));
+console.log(dbInterface.addPerson(dbConnection, 'John Deere', 'weed@wha.ck', 7605555555, 'grass', {'frontend': true, 'backend': true, 'machineLearn': true}, {'java': true}));
 
 // console.log(emails);
 dbConnection.end();
