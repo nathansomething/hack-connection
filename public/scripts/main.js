b297053da1f1b6f70c20870bfa8f5a1dbb0294a8
Angela Massey <amassey@bu.edu>
2016-01-24 07:36:40 -0500

    Added  ion.rangeslider with accompanying img,stylesheets,and js code.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
-	//alert("hi");
+$(document).ready(function(){
+	$("#CompetitiveRating").ionRangeSlider({
+		values:["I'm just here to have fun!", "Working on the ferocity","The Middle Path", "Working on the chill", 
+		"I'm in it to win it."],
+		from:"The Middle Path"
+	});
 });
